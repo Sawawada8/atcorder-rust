f90a0b84a9cd9bad5f4d4e0659a6e6f0c6f9c8c8
Sawawada8 <sawadadamskz7575@gmail.com>
2024-02-20 09:43:23 +0900

    add bin statement on toml

--- sh/m.sh
+++ sh/m.sh
@@ -15,4 +15,17 @@ cp src/main.rs ${pa}"/B/main.rs"
 cp src/main.rs ${pa}"/C/main.rs"
 cp src/main.rs ${pa}"/D/main.rs"
 
+echo [[bin]] >> Cargo.toml
+echo name = \"$1"-"$2"-a\"" >> Cargo.toml
+echo "path = \"src/$1/$2/A/main.rs\"" >> Cargo.toml
+echo [[bin]] >> Cargo.toml
+echo name = \"$1"-"$2"-b\"" >> Cargo.toml
+echo "path = \"src/$1/$2/B/main.rs\"" >> Cargo.toml
+echo [[bin]] >> Cargo.toml
+echo name = \"$1"-"$2"-c\"" >> Cargo.toml
+echo "path = \"src/$1/$2/C/main.rs\"" >> Cargo.toml
+echo [[bin]] >> Cargo.toml
+echo name = \"$1"-"$2"-d\"" >> Cargo.toml
+echo "path = \"src/$1/$2/D/main.rs\"" >> Cargo.toml
+
 echo "💫_complete"
